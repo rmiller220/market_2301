36efd3e0d6121d4e5162ea195cb0427be7853e4d
Reilly Miller <reillyjmiller31@gmail.com>
2023-03-07 11:59:00 -0700

    test

--- lib/market.rb
+++ lib/market.rb
@@ -1,3 +1,56 @@
 class Market
+  attr_reader :name, :vendors
+  def initialize(name)
+    @name = name
+    @vendors = []
+    # @total_inventory = Hash.new(0)
+  end
+
+  def add_vendor(vendor)
+    @vendors << vendor
+  end
+
+  def vendor_names
+    v_names = []
+    @vendors.each do |vendor|
+      v_names << vendor.name
+    end
+    v_names
+  end
+
   
+  def vendors_that_sell(item)
+    v_sell_item = []
+    @vendors.each do |vendor|
+      if vendor.inventory.include?(item)
+        v_sell_item << vendor
+      end
+    end
+    v_sell_item
+  end
+  
+  def total_inventory
+    inventory = Hash.new { |hash, key| hash[key] = { quantity: 0, vendors: [] } }
+    @vendors.each do |vendor|
+      vendor.inventory.each do |item, quantity|
+        inventory[item][:quantity] += quantity
+        inventory[item][:vendors] << vendor
+      end
+    end
+    inventory
+  end
+  def overstocked_items
+    overstocked = []
+    total_inventory.each do |item, data|
+      if data[:quantity] > 500 && data[:vendors].size > 1
+        overstocked << item
+      end
+    end
+    overstocked
+  end
+
+  def sorted_item_list
+    items = @vendors.flat_map { |vendor| vendor.inventory.keys.map(&:name) }
+    items.uniq.sort
+  end
 end
--- lib/vendor.rb
+++ lib/vendor.rb
@@ -1,3 +1,23 @@
 class Vendor
-  
+  attr_reader :name, :inventory
+  def initialize(name)
+    @name = name
+    @inventory = Hash.new(0)
+  end
+
+  def check_stock(item)
+    @inventory[item] 
+  end
+
+  def stock(item, num)
+    @inventory[item] += num
+  end
+
+  def potential_revenue
+    revenue = 0
+    @inventory.each do |item, num|
+      revenue += item.price * num
+    end
+    revenue
+  end
 end
